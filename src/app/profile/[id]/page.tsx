45f3a69cbfb8010394eb16136116cbc01a6bbd3e
rahul karadwal <rahulkaradwal2000@gmail.com>
2025-01-10 12:01:05 -0500

    feat: profile page ui and clerk initialization

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,13 +1,34 @@
 import Feed from "@/components/feed/Feed";
-import UserPostInfo from "@/components/feed/UserPostInfo";
+import Navigation from "@/components/leftSideBar/Navigation";
 import ProfileCard from "@/components/profile/ProfileCard";
+import UserInformation from "@/components/profile/UserInformation";
+import UserMediaCard from "@/components/profile/UserMediaCard";
+import Birthday from "@/components/rightSideBar/birthday/Birthday";
+import FriendRequest from "@/components/rightSideBar/friendRequest/FriendRequest";
+import SponsoredAds from "@/components/rightSideBar/sponsoredAds/SponsoredAds";
 import React from "react";
 
 function page() {
   return (
-    <main className="bg-gray-100 overflow-x-scroll pt-6 flex gap-6  flex-col ">
-      <ProfileCard />
-      <Feed />
+    <main className="bg-gray-100 overflow-x-scroll pt-6 flex gap-6   ">
+      <div className="hidden md:block w-[20%]  ">
+        <div className="flex flex-col gap-6 ">
+          <Navigation />
+          <SponsoredAds size="sm" />
+        </div>
+      </div>
+      <div className="md:w-[70%] lg:w-[50%] w-full ">
+        <ProfileCard />
+        <Feed />
+      </div>
+
+      <div className="hidden w-[30%] lg:flex flex-col  gap-6 ">
+        <UserInformation />
+        {/* <UserMediaCard userId="rahulkardwal" /> */}
+        <FriendRequest />
+        <Birthday />
+        <SponsoredAds size="sm" />
+      </div>
     </main>
   );
 }
--- src/components/rightSideBar/friendRequest/FriendRequest.tsx
+++ src/components/rightSideBar/friendRequest/FriendRequest.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Request from "./Request";
+import Link from "next/link";
 
 function FriendRequest() {
   return (
     <div className="bg-white shadow-md w-full  p-4 rounded-lg">
       <div className="flex justify-between items-center mb-4">
         <p>Friend Requests</p>
-        <button className="text-sm text-blue-500">See all</button>
+        <Link href="/" className="text-blue-500 text-xs">
+          See all
+        </Link>
       </div>
       <div className="flex flex-col gap-4">
         <Request />

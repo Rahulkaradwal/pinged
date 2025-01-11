73b075f6215b8091056cf26c6fd5ec9ab47bc7ef
rahul karadwal <rahulkaradwal2000@gmail.com>
2025-01-11 14:28:01 -0500

    feat: intigrate mysql using docker

--- src/components/header/MenuButtons.tsx
+++ src/components/header/MenuButtons.tsx
@@ -1,38 +1,54 @@
 import Image from "next/image";
 import React from "react";
 import HamburgerButton from "./HamburgerButton";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import Link from "next/link";
 
 function MenuButtons() {
   return (
     <div className=" justify-end  xl:w-[25%]  gap-8  flex items-center ">
-      <Image
-        src="/people.png"
-        alt="logo"
-        width={22}
-        height={22}
-        className="cursor-pointer"
-      />
-      <Image
-        src="/messages.png"
-        alt="logo"
-        width={22}
-        height={22}
-        className="cursor-pointer"
-      />
-      <Image
-        src="/notifications.png"
-        alt="logo"
-        width={22}
-        height={22}
-        className="cursor-pointer"
-      />
-      <Image
-        src="/notifications.png"
-        alt="logo"
-        width={22}
-        height={22}
-        className="cursor-pointer"
-      />
+      <ClerkLoading>
+        <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignedIn>
+          <Image
+            src="/people.png"
+            alt="logo"
+            width={22}
+            height={22}
+            className="cursor-pointer"
+          />
+          <Image
+            src="/messages.png"
+            alt="logo"
+            width={22}
+            height={22}
+            className="cursor-pointer"
+          />
+          <Image
+            src="/notifications.png"
+            alt="logo"
+            width={22}
+            height={22}
+            className="cursor-pointer"
+          />
+        </SignedIn>
+        <SignedOut>
+          <div className="flex items-center gap-2 text-sm">
+            <Image src="/login.png" alt="" width={20} height={20} />
+            <Link href="/sign-in">Login/Register</Link>
+          </div>
+        </SignedOut>
+      </ClerkLoaded>
+
+      <UserButton />
       <HamburgerButton />
     </div>
   );
--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Logo from "./Logo";
 import MenuButtons from "./MenuButtons";
 import Features from "./Features";
+import { ClerkLoading } from "@clerk/nextjs";
 
 function NavBar() {
   return (
     <nav className="h-24 py-4 px-4  md:px-9 lg:px-16  xl:px-32 gap-4  shadow-sm flex justify-between items-center  ">
       <Logo />
       <Features />
+
       <MenuButtons />
     </nav>
   );
